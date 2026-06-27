cac7e21bf993e8e46aa3f9ec8407b203c363c10a
davidmelo021 <137961611+davidmelo021@users.noreply.github.com>
2026-06-26 23:27:34 -0300

    andamento do perfil

--- src/pages/perfil/index.tsx
+++ src/pages/perfil/index.tsx
@@ -38,6 +38,13 @@ export default function Perfil () {
     nome: '',
     email: '',
     telefone: '', 
+    cep: '',
+    rua: '',
+    numero: '',
+    bairro: '',
+    cidade: '',
+    estado: '',
+    metodo_pagamento: '',
     
   });
 
