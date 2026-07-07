ac952b26ff6e82d428517595a6d18e7b45eecb01
davidmelo021 <137961611+davidmelo021@users.noreply.github.com>
2026-07-07 12:30:09 -0300

    Consertando detallhes

--- src/App.tsx
+++ src/App.tsx
@@ -2,46 +2,47 @@ import { useState } from 'react';
 import Header from './components/Header';
 import { Sidebar } from './components/Menu';
 import { AppRoutes } from './routes/routes';
-import { LoginButton } from './components/LoginButton'; 
+import { LoginButton } from './components/LoginButton';
 import { BrowserRouter } from 'react-router-dom';
 import { CartProvider } from './context/CartContext';
-import  CartSidebar  from './components/CartSidebar';
+import CartSidebar from './components/CartSidebar';
 import { AuthProvider } from './context/AuthContext';
 
-
-function App() {
+function AppContent() {
   const [openMenu, setOpenMenu] = useState(false);
   const [openCart, setOpenCart] = useState(false);
-  
+
   const toggleMenu = () => setOpenMenu(!openMenu);
   const toggleCart = () => setOpenCart(!openCart);
 
-  
+  return (
+    <>
+      <Header toggleMenu={toggleMenu} toggleCart={toggleCart} />
+      <CartSidebar open={openCart} toggleCart={toggleCart} />
+      <Sidebar open={openMenu} />
+      <div
+        style={{
+          marginTop: '70px',
+          marginLeft: openMenu ? '220px' : '0',
+          transition: 'margin-left 0.3s ease',
+          overflowX: 'hidden',
+        }}
+      >
+        <AppRoutes />
+      </div>
+      <LoginButton />
+    </>
+  );
+}
+
+function App() {
   return (
     <AuthProvider>
-    <BrowserRouter>
-      <CartProvider>
-        <Header toggleMenu={toggleMenu} toggleCart={toggleCart} />
-        <CartSidebar open={openCart} toggleCart={toggleCart} />
-
-        <Sidebar open={openMenu} />
-       
-
-        <div
-          style={{
-            marginTop: '70px',
-            marginLeft: openMenu ? '220px' : '0',
-            transition: 'margin-left 0.3s ease',
-            overflowX: 'hidden',
-          }}
-        >
-          <AppRoutes />
-        </div>
-
-        <LoginButton />
-      </CartProvider>
-    </BrowserRouter>
-
+      <BrowserRouter>
+        <CartProvider>
+          <AppContent />
+        </CartProvider>
+      </BrowserRouter>
     </AuthProvider>
   );
 }
